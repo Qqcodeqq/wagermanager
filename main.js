34878fa067c0a0977430e75b7d3d8fe91c655889
Qqcodeqq <119717141+Qqcodeqq@users.noreply.github.com>
2023-04-10 17:40:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // main.js
-const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"approveEmergencyWithdrawal","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"claim","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"claimJudgeFee","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_judge","type":"address"},{"internalType":"uint256","name":"_wagerAmount","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"emergencyWithdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"emergencyWithdrawalApproved","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"finalOutcome","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"participant","type":"address"}],"name":"isDeposited","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"judge","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"outcome","type":"uint256"}],"name":"judgeDecision","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"judgeFeeClaimed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"outcomes","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"participant1","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"participant1Deposited","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"participant2","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"participant2Deposited","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"resetContract","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"outcome","type":"uint256"}],"name":"submitOutcome","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"wagerAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]; // Replace <ABI_JSON> with the ABI you provided
-const contractAddress = '0x3e06f9518A446a127A5bB72011C4EDFF268F13b4'; // Replace with your contract address
-const providerUrl = 'https://nova.arbitrum.io/rpc';
+
+const contractABI = [{"inputs":[...]}]; // Include the complete ABI here
+const contractAddress = "YOUR_CONTRACT_ADDRESS";
+const rpcURL = "YOUR_RPC_URL"; // Replace with your RPC URL, for example: https://mainnet.infura.io/v3/YOUR-PROJECT-ID
 
 let web3;
 let contract;
@@ -56,10 +57,47 @@ async function initializeUI() {
     }
   });
 
+  document.getElementById("claimBtn").addEventListener("click", async () => {
+    try {
+      await contract.methods.claim().send({ from: userAddress });
+      alert("Claim successful");
+    } catch (error) {
+      console.error("Error while claiming:", error);
+    }
+  });
+
+  document.getElementById("p1WinBtn").addEventListener("click", async () => {
+    try {
+      await contract.methods.submitOutcome(1).send({ from: userAddress });
+      alert("Outcome submitted: Participant 1 wins");
+    } catch (error) {
+      console.error("Error while submitting outcome:", error);
+    }
+  });
+
+  document.getElementById("p1LoseBtn").addEventListener("click", async () => {
+    try {
+      await contract.methods.submitOutcome(2).send({ from: userAddress });
+      alert("Outcome submitted: Participant 1 loses");
+    } catch (error) {
+      console.error("Error while submitting outcome:", error);
+    }
+  });
+
+  document.getElementById("p2WinBtn").addEventListener("click", async () => {
+    try {
+      await contract.methods.submitOutcome(2).send({ from: userAddress });
+      alert("Outcome submitted: Participant 2 wins");
+    } catch (error) {
+      console.error("Error while submitting outcome:", error);
+    }
+  });
+
   // Fetch contract information
   const owner = await contract.methods.owner().call();
   const judge = await contract.methods.judge().call();
-  const participant1 = await contract.methods.participant1().call();
+  const participant1 = await contract.methods.participant1().call
+  Copy code
   const participant2 = await contract.methods.participant2().call();
   const wagerAmount = await contract.methods.wagerAmount().call();
   const wagerAmountInEther = web3.utils.fromWei(wagerAmount, "ether");
